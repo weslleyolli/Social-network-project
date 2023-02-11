850748d94c7f0bae0c9852947553b74a207f2618
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-11 16:01:59 -0300

    Project finished

--- 01-fundamentals-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentals-reactjs-ts/src/components/Avatar.tsx
@@ -1,18 +1,19 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css'
 
 
-interface AvatarProps {
+interface AvatarProps  extends ImgHTMLAttributes<HTMLImageElement>{
     hasBorder?: boolean;
     src: string;
 }
 
-export function Avatar({hasBorder = true, src}: AvatarProps) {
+export function Avatar({hasBorder = true, ...props}: AvatarProps) {
 
 
     return (
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-            src={src} 
+            {...props} 
         />
     )
 }
